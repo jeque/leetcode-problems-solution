497550a932bcc9a401a5ac00a62e3f81d0257205
jeque <jeque123@163.com>
2018-10-16 22:22:58 +0800

    2018.10.16
    2018.10.16

---
+++ 28. Implement strStr.py
@@ -0,0 +1,13 @@
+class Solution:
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        if needle in haystack:
+            return haystack.index(needle)
+        elif len(needle) < 1:
+            return 0
+        else:
+            return -1
