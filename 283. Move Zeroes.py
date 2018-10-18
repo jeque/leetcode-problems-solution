e017c8bd6e22219c4d79b12f56602229b7dca057
jeque <jeque123@163.com>
2018-10-18 22:09:17 +0800

    2018.10.18
    2018.10.18

---
+++ 283. Move Zeroes.py
@@ -0,0 +1,15 @@
+class Solution:
+    def moveZeroes(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        p = 0
+        for i in range(len(nums)):
+            if nums[i] == 0:
+                p += 1
+                continue
+            nums[i-p] = nums[i]
+        #    nums[p] == 0
+        for i in range(len(nums[:-p]),len(nums)):
+            nums[i] = 0
